4a51db3b8126c131cc2042940b0c7a1a13247b9d
samucj73 <samu.rcj@gmail.com>
2025-05-30 23:31:25 -0300

    Update lotofacil_avancado.py

--- lotofacil_avancado.py
+++ lotofacil_avancado.py
@@ -28,37 +28,36 @@ class LotoFacilAvancado:
         return dict(sorted(contagem.items()))
 
     def gerar_cartoes_com_avancado(self, num_cartoes=5, alvo_min_acertos=13):
-    cartoes_validos = []
-    candidatos = []
+        cartoes_validos = []
+        candidatos = []
 
-    tentativas = 0
-    max_tentativas = num_cartoes * 1300
+        tentativas = 0
+        max_tentativas = num_cartoes * 1300
 
-    media_primos = round(self.media_primos())
-    media_multiplos = round(self.media_multiplos_3())
+        media_primos = round(self.media_primos())
+        media_multiplos = round(self.media_multiplos_3())
 
-    while len(cartoes_validos) < num_cartoes and tentativas < max_tentativas:
-        dezenas = sorted(random.sample(range(1, 26), 15))
-        acertos_simulados = self.simular_desempenho(dezenas)
-        qt_primos = len([d for d in dezenas if d in self.primos()])
-        qt_multiplos = len([d for d in dezenas if d in self.multiplos_3()])
+        while len(cartoes_validos) < num_cartoes and tentativas < max_tentativas:
+            dezenas = sorted(random.sample(range(1, 26), 15))
+            acertos_simulados = self.simular_desempenho(dezenas)
+            qt_primos = len([d for d in dezenas if d in self.primos()])
+            qt_multiplos = len([d for d in dezenas if d in self.multiplos_3()])
 
-        if (media_primos - 1 <= qt_primos <= media_primos + 1) and \
-           (media_multiplos - 1 <= qt_multiplos <= media_multiplos + 1):
-            candidatos.append((acertos_simulados, dezenas))
-            if acertos_simulados >= alvo_min_acertos:
-                cartoes_validos.append(dezenas)
+            if (media_primos - 1 <= qt_primos <= media_primos + 1) and \
+               (media_multiplos - 1 <= qt_multiplos <= media_multiplos + 1):
+                candidatos.append((acertos_simulados, dezenas))
+                if acertos_simulados >= alvo_min_acertos:
+                    cartoes_validos.append(dezenas)
 
-        tentativas += 1
+            tentativas += 1
 
-    if cartoes_validos:
-        return cartoes_validos
-    else:
-        # Fallback: retorna os melhores candidatos encontrados
-        candidatos.sort(reverse=True)  # ordena pelos que mais acertaram
-        return [d for _, d in candidatos[:num_cartoes]]
+        if cartoes_validos:
+            return cartoes_validos
+        else:
+            # Fallback: retorna os melhores candidatos encontrados
+            candidatos.sort(reverse=True)  # ordena pelos que mais acertaram
+            return [d for _, d in candidatos[:num_cartoes]]
 
-    
     def simular_desempenho(self, cartao):
         # Simula acertos do cartão nos últimos 20 concursos
         historico = self.concursos[:20]
